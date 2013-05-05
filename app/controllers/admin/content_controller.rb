b2b4d3b36c898ec34a55d998f798c8c8a30f4956
Christopher Moseley <cs@csmoseley.com>
2013-05-05 10:55:13 -0400

    HW5 complete???

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -8,7 +8,7 @@ class Admin::ContentController < Admin::BaseController
 
 	def merge_with
 		article = Article.find_by_id(params[:id])
-		if article.merge_with(params[:merge_with])
+		if article.merge_articles(params[:merge_with])
       flash[:notice] = _("Articles Merged")
 			redirect_to :action => :edit, :id => params[:id]
     else
